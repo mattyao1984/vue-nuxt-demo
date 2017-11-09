32aed738079bf17f15e5dc0160cb289a2d3e0548
Matt Yao <mattyao1984@gmail.com>
2017-11-09 22:50:20 +1100

    Init project with nuxt and axios

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="f1 code">
+    Hello World!
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{user.login}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import axios from '~/plugins/axios';
+
+  export default {
+    asyncData() {
+      return axios.get('users')
+        .then(res => ({
+          users: res.data
+        }));
+    }
+  };
+</script>
